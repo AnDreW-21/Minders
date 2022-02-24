c174a74fb0949d317f5975d643741a36806e710a
andrew <andrewnasief3.com>
2022-02-24 21:15:31 +0200

    first task

---
+++ bin/untitled.dart
@@ -0,0 +1,136 @@
+import 'dart:io';
+
+void startSystem() {
+  print('1-add User');
+  print('2-delete User');
+  print('3-search by id');
+  print('4-search by name');
+  print('5-print all');
+}
+
+Map addUser(var id, String name, int age, String faculty) {
+  Map user = {'id': id, 'name': name, 'age': age, 'faculty': faculty};
+  return user;
+}
+
+int searchForID(List<Map> lists, var size, int id) {
+  if (size < 0) {
+    return -1;
+  } else if (lists[size]['id'] == id) {
+    return size;
+  } else {
+    return searchForID(lists, size - 1, id);
+  }
+}
+
+void printALL(List<Map> lists) {
+  for (int i = 0; i < lists.length; i++) {
+    print(lists[i]);
+  }
+}
+
+void deleteMember(int id, List<Map> person) {
+  person.removeWhere((item) => item['id'] == id);
+}
+
+List<Map> selectionSort(List<Map> person, int n) {
+  List<Map> list=person;
+  int min;
+  for (int i = 0; i < n - 1; i++) {
+    min = i;
+    for (var j = i + 1; j < n; j++) {
+      if (list[j]['id'] < list[min]['id']) {
+        min = j;
+      }
+    }
+    Map damy = list[min];
+    list[min] = list[i];
+    list[i] = damy;
+  }
+  return list;
+}
+
+int binarySearch(List<Map> person, int n, String name) {
+  int left = 0;
+  int right = n - 1;
+  while (left <= right) {
+    int m = (left + (right - left) / 2).toInt();
+    if (name == (person[m]['name'])) {
+      return m;
+    }
+
+    // If x greater, ignore left half
+    if (person[m]['name'] < name) {
+      left = m + 1;
+    } else {
+      right = m - 1;
+    }
+  }
+
+  return -1;
+}
+
+void main(List<String> arguments) {
+  List<Map> person = [
+    {'id': 3, 'name': 'Andrew', 'age': '20', 'faculty': 'FCAI'},
+    {'id': 2, 'name': 'Nossa', 'age': '22', 'faculty': 'AUC'}
+  ];
+  while (true) {
+    startSystem();
+    int x = int.parse(stdin.readLineSync()!);
+    switch (x) {
+      case 1:
+        print("Enter your ID");
+        var id = int.parse(stdin.readLineSync()!);
+        print("Enter your name");
+        String name = stdin.readLineSync()!;
+        print("Enter your age");
+        int age = int.parse(stdin.readLineSync()!);
+        print("Enter your faculty");
+        String faculty = stdin.readLineSync()!;
+        person.add(addUser(id, name, age, faculty));
+        break;
+      case 2:
+        print("Enter your ID");
+        var id = int.parse(stdin.readLineSync()!);
+        deleteMember(id, person);
+        break;
+      case 3:
+        print("Enter your ID");
+        var id = int.parse(stdin.readLineSync()!);
+        int index = searchForID(person, person.length - 1, id);
+        if (index < 0) {
+          print('Not Found');
+        } else {
+          print(person[index]);
+        }
+        break;
+      case 4:
+        String name = stdin.readLineSync()!;
+        int index = binarySearch(
+            selectionSort(person, person.length), person.length - 1, name);
+        if (index == -1) {
+          print("NOT FOUND");
+        } else {
+          print(person[index]);
+        }
+        break;
+      case 5:
+        print('1-sorted');
+        print('2-unsorted');
+        String state = stdin.readLineSync()!;
+        if (state == '1') {
+          printALL(selectionSort(person, person.length));
+        } else {
+          printALL(person);
+        }
+        break;
+      default:
+        print('ERROR NOT FOUNd');
+    }
+    print('want to do anything else?(y/n)');
+    String state = stdin.readLineSync()!;
+    if (state.toLowerCase() == 'n') break;
+  }
+  print('Thank you \u2764');
+}
